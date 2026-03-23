95acd4a6d802572205370ef460e90e92ba97d83e
DuKemet <vedantnaikwadi1@gmail.com>
2026-03-23 22:39:44 +0530

    phone number and email

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -69,19 +69,12 @@ export default function Footer() {
                                     </span>
                                 </a>
                             </li>
-                            {/* <li>
-                                <a href="mailto:submissions@sanidhya.edu">
-                                    <span style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
-                                        <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M14 2H6a2 2 0 00-2 2v16a2 2 0 002 2h12a2 2 0 002-2V8z" /><path d="M14 2v6h6M16 13H8M16 17H8M10 9H8" /></svg>
-                                        submissions@sanidhya.edu
-                                    </span>
-                                </a>
-                            </li> */}
+                            
                             <li>
                                 <a href="tel:+919324551462">
                                     <span style={{ display: 'flex', alignItems: 'center', gap: 8 }}>
                                         <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z" /></svg>
-                                        +91-9324551462
+                                        +91 9324551462
                                     </span>
                                 </a>
                             </li>
