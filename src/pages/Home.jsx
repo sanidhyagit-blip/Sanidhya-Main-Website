04d99b15c1f81a2427717de40c85481f34f1b290
DuKemet <vedantnaikwadi1@gmail.com>
2026-07-05 09:59:54 +0530

    UseRef error solve

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useCallback } from 'react'
+import { useState, useMemo, useCallback, useRef } from 'react'
 import { Link } from 'react-router-dom'
 import useReveal from '../hooks/useReveal'
 
